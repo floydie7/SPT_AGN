69115f93d9c3b88b052c5d7416704515d9c8b903
Benjamin Floyd <benjaminfloyd7@gmail.com>
2020-05-04 17:23:11 -0500

    Modifications to the MCMC plotting script for SPTcl chains.

--- MCMC_Generate_Plots_Test_Suite.py
+++ MCMC_Generate_Plots_Test_Suite.py
@@ -4,7 +4,6 @@ Author: Benjamin Floyd
 
 Generates the chain walker and corner plots for a previously generated emcee chain file.
 """
-import re
 
 import corner
 import emcee
@@ -13,33 +12,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import MaxNLocator
 
-# Set parameter values
-# theta_true = 2.5  # Amplitude.
-# eta_true = 1.2  # Redshift slope
-# zeta_true = -1.0  # Mass slope
-beta_true = 1.0  # Radial slope
-rc_true = 0.1  # Core radius (in r500)
-C_true = 0.371  # Background AGN surface density
-
-max_radius = 5.0  # Maximum integration radius in r500 units
+labels = [r'$\theta$', r'$\eta$', r'$\zeta$', r'$\beta$', r'$r_c$', r'$C$']
 
 # Our file storing the full test suite
-filename = 'Data/MCMC/Mock_Catalog/Chains/Final_tests/slope_tests/' \
-           'emcee_run_w36_s1000000_mock_tvariable_evariable_zvariable_b1.0_rc_0.1_C0.371_slope_tests.h5'
+filename = 'Data/MCMC/SPT_Data/Chains/emcee_run_w36_s1000000_SPTcl_IRAGN.h5'
 
 # Get a list of the chain runs stored in our file
 with h5py.File(filename, 'r') as f:
     chain_names = list(f.keys())
-
-chain_names = [chain_name for chain_name in chain_names if 'trial5' in chain_name]
 
 # Load in all samplers from the file
 sampler_dict = {chain_name: emcee.backends.HDFBackend(filename, name=chain_name) for chain_name in chain_names}
 
 # Process each chain
 for chain_name, sampler in sampler_dict.items():
-    print('-----\n{}'.format(chain_name))
-    theta_true, eta_true, zeta_true = np.array(re.findall(r'-?\d+(?:\.\d+)', chain_name), dtype=np.float)
+    print(f'-----\n{chain_name}')
 
     # Get the chain from the sampler
     samples = sampler.get_chain()
@@ -47,30 +34,18 @@ for chain_name, sampler in sampler_dict.items():
     # To get the number of iterations ran, number of walkers used, and the number of parameters measured
     nsteps, nwalkers, ndim = samples.shape
 
-    labels = [r'$\theta$', r'$\eta$', r'$\zeta$', r'$\beta$', r'$r_c$', r'$C$']
-    truths = [theta_true, eta_true, zeta_true, beta_true, rc_true, C_true]
-
     # Plot the chains
     fig, axes = plt.subplots(nrows=ndim, ncols=1, sharex='col')
     for i in range(ndim):
         ax = axes[i]
         ax.plot(samples[:, :, i], color='k', alpha=0.3)
-        ax.axhline(truths[i], color='b')
         ax.yaxis.set_major_locator(MaxNLocator(5))
         ax.set(xlim=[0, len(samples)], ylabel=labels[i])
 
     axes[0].set(title=chain_name)
     axes[-1].set(xlabel='Steps')
 
-    fig.savefig('Data/MCMC/Mock_Catalog/Plots/Final_tests/Slope_tests/trial_5/mcmc_results/'
-                'Param_chains_mock_t{theta}_e{eta}_z{zeta}_b{beta}_rc{rc}_C{C}_{chain_name}_full_spt.pdf'
-                .format(theta=theta_true, eta=eta_true, zeta=zeta_true, beta=beta_true, rc=rc_true, C=C_true,
-                        chain_name=chain_name),
-                format='pdf')
-
-    # These chains have problems with calculating the autocorrelation. Skip making the corner plot.
-    if chain_name in ['trial2_t4.711_e0.00_z-1.00', 'trial3_t7.816_e0.00_z-1.50', 'trial4_t4.711_e0.00_z-1.00']:
-        continue
+    fig.savefig(f'Data/MCMC/SPT_Data/Plots/Param_chains_SPTcl_{chain_name}.pdf', format='pdf')
 
     try:
         # Calculate the autocorrelation time
@@ -97,23 +72,20 @@ for chain_name, sampler in sampler_dict.items():
     # labels[-1] = r'$\ln(Post)$'
 
     # Produce the corner plot
-    fig = corner.corner(flat_samples, labels=labels, truths=truths, quantiles=[0.16, 0.5, 0.84], show_titles=True)
+    fig = corner.corner(flat_samples, labels=labels, quantiles=[0.16, 0.5, 0.84], show_titles=True)
     fig.suptitle(chain_name)
-    fig.savefig('Data/MCMC/Mock_Catalog/Plots/Final_tests/Slope_tests/trial_5/mcmc_results/'
-                'Corner_plot_mock_t{theta}_e{eta}_z{zeta}_b{beta}_rc{rc}_C{C}_{chain_name}_full_spt.pdf'
-                .format(theta=theta_true, eta=eta_true, zeta=zeta_true, beta=beta_true, rc=rc_true, C=C_true,
-                        chain_name=chain_name), format='pdf')
+    fig.savefig(f'Data/MCMC/SPT_Data/Plots/Corner_plot_SPTcl_{chain_name}.pdf', format='pdf')
 
-    print('Iterations ran: {}'.format(sampler.iteration))
+    print(f'Iterations ran: {sampler.iteration}')
     for i in range(ndim):
         mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
         q = np.diff(mcmc)
-        print('{labels} = {median:.3f} +{upper_err:.4f} -{lower_err:.4f} (truth: {true})'
-              .format(labels=labels[i].strip('$\\'), median=mcmc[1], upper_err=q[1], lower_err=q[0], true=truths[i]))
+        print('{labels} = {median:.3f} +{upper_err:.4f} -{lower_err:.4f}'
+              .format(labels=labels[i].strip('$\\'), median=mcmc[1], upper_err=q[1], lower_err=q[0]))
 
-    print('Mean acceptance fraction: {:.2f}'.format(np.mean(sampler.accepted / sampler.iteration)))
+    print(f'Mean acceptance fraction: {np.mean(sampler.accepted / sampler.iteration):.2f}')
 
     # Get estimate of autocorrelation time
-    print('Autocorrelation time: {:.1f}'.format(tau))
+    print(f'Autocorrelation time: {tau:.1f}')
     plt.close('all')
 # plt.show()
