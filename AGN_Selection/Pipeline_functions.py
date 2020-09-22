Pipeline_functions.py:  - Switch from QUADPACK quad to adaptive quadrature for purification probability normalization..  - Fix the purification probability integrand function to actually return the values.  - Adjusted the duplicate check table in image_to_catalog_match to avoid depreciation warning.  - Removed the `is_cluster` flag from object_separations function. Will handle this in post processing rather than complicate the selector code. SDWFS_Cutout_AGN_Pipeline.py:  - Added object separation post-processing.  - Fixed the filename of the number count distribution file to actually point to a file.  - Fixed requested output catalog column names.  - Minor fixes to post-success statistics print out.

AGN_Selection/Pipeline_functions.py
@@ -25,7 +25,7 @@ from astropy.wcs import WCS
 from matplotlib.patches import Ellipse
 from matplotlib.path import Path
 from matplotlib.transforms import Affine2D
-from scipy.integrate import quad, quad_vec
+from scipy.integrate import quad_vec, quadrature
 from scipy.interpolate import interp1d
 from scipy.stats import norm
 from synphot import SourceSpectrum, SpectralElement
@@ -214,8 +214,8 @@ class SelectIRAGN:
 
         # If there are any duplicate matches in the sample remaining we need to remove the match that is the poorer
         # match. We will only keep the closest matches.
-        match_info = Table(np.array([[cluster['SPT_cat_idx'], cluster['center_sep'], cluster_id]
-                                     for cluster_id, cluster in self._catalog_dictionary.items()]),
+        match_info = Table(rows=[[cluster['SPT_cat_idx'], cluster['center_sep'], cluster_id]
+                                 for cluster_id, cluster in self._catalog_dictionary.items()],
                            names=['SPT_cat_idx', 'center_sep', 'cluster_id'])
 
         # Sort the table by the catalog index.
@@ -511,7 +511,8 @@ class SelectIRAGN:
             # our AGN
             if 'k-correction' in cluster_info:
                 dist_modulus = self._cosmo.distmod(cluster_info['redshift']).value
-                sex_catalog['AGN_ABSOLUTE_MAG'] = sex_catalog['I2_MAG_APER4'] - dist_modulus - cluster_info['k-correction']
+                sex_catalog['AGN_ABSOLUTE_MAG'] = sex_catalog['I2_MAG_APER4'] - dist_modulus - cluster_info[
+                    'k-correction']
 
                 # Using the absolute magnitudes, further refine the IR-bright selection to only include AGN that have
                 # intrinsic brightnesses above a given threshold. This will insure that we have a fair sample across our
@@ -570,7 +571,8 @@ class SelectIRAGN:
         color_probability_distribution = interp1d(color_bins, field_number_counts)
 
         # For the probability computed later for each object, find the normalization factor
-        color_prob_in_denom = quad(color_probability_distribution, a=ch1_ch2_color_cut, b=color_bins.max())[0]
+        color_prob_in_denom = quadrature(color_probability_distribution,
+                                         a=ch1_ch2_color_cut, b=np.max(color_bins), maxiter=10000)[0]
 
         clusters_to_remove = []
         for cluster_id, cluster_info in self._catalog_dictionary.items():
@@ -586,10 +588,10 @@ class SelectIRAGN:
 
             # Convolve the error distribution for each object with the overall number count distribution
             def object_integrand(x):
-                norm(loc=I1_I2_color, scale=I1_I2_color_err).pdf(x) * color_probability_distribution(x)
+                return norm(loc=I1_I2_color, scale=I1_I2_color_err).pdf(x) * color_probability_distribution(x)
 
             # Compute the probability contained within the selection region by each object's color error
-            color_prob_in_numer = quad_vec(object_integrand, a=ch1_ch2_color_cut, b=color_bins.max())[0]
+            color_prob_in_numer = quad_vec(object_integrand, a=ch1_ch2_color_cut, b=np.max(color_bins))[0]
             color_prob_in = color_prob_in_numer / color_prob_in_denom
 
             # Draw a random number for rejection sampling
@@ -645,17 +647,11 @@ class SelectIRAGN:
 
             cluster_info['catalog'] = sex_catalog
 
-    def object_separations(self, is_cluster=True):
+    def object_separations(self):
         """
         Calculates the separations of each object relative to the SZ center.
 
         Finds both the angular separations and physical separations relative to the cluster's r500 radius.
-
-        Parameters
-        ----------
-        is_cluster : bool, optional
-            Boolean flag that will allow skipping of the r500 relative separations. Used for field catalogs. Defaults to
-            `True`.
 
         """
 
@@ -669,19 +665,18 @@ class SelectIRAGN:
             # Calculate the angular separations between the objects and the SZ center in arcminutes
             separations_arcmin = object_coords.separation(sz_center).to(u.arcmin)
 
-            if is_cluster:
-                # Compute the r500 radius for the cluster
-                r500 = (3 * catalog['M500'][0] * u.Msun /
-                        (4 * np.pi * 500 * self._cosmo.critical_density(catalog['REDSHIFT'][0]).to(
-                            u.Msun / u.Mpc ** 3))) ** (1 / 3)
+            # Compute the r500 radius for the cluster
+            r500 = (3 * catalog['M500'][0] * u.Msun /
+                    (4 * np.pi * 500 * self._cosmo.critical_density(catalog['REDSHIFT'][0]).to(
+                        u.Msun / u.Mpc ** 3))) ** (1 / 3)
 
-                # Convert the angular separations into physical separations relative to the cluster's r500 radius
-                separations_r500 = (separations_arcmin / r500
-                                    * self._cosmo.kpc_proper_per_arcmin(catalog['REDSHIFT'][0]).to(u.Mpc / u.arcmin))
+            # Convert the angular separations into physical separations relative to the cluster's r500 radius
+            separations_r500 = (separations_arcmin / r500
+                                * self._cosmo.kpc_proper_per_arcmin(catalog['REDSHIFT'][0]).to(u.Mpc / u.arcmin))
 
-                # Add our new columns to the catalog
-                catalog['R500'] = r500
-                catalog['RADIAL_SEP_R500'] = separations_r500
+            # Add our new columns to the catalog
+            catalog['R500'] = r500
+            catalog['RADIAL_SEP_R500'] = separations_r500
             catalog['RADIAL_SEP_ARCMIN'] = separations_arcmin
 
             # Update the catalog in the data structure

AGN_Selection/SDWFS_Cutout_AGN_Pipeline.py
@@ -8,6 +8,8 @@ A unified IR-AGN selection pipeline for both the SPT-SZ + SPTpol 100d surveys.
 from time import time
 
 import astropy.units as u
+import numpy as np
+from astropy.coordinates import SkyCoord
 from astropy.table import Table
 
 from AGN_Selection.Pipeline_functions import SelectIRAGN
@@ -31,7 +33,8 @@ sdfws_master_cutout_catalog = Table.read(f'{prefix}Data/Data_Repository/Project_
                                          'SDWFS_Cutout_master.fits')
 
 # SDWFS number count distribution file (for purification)
-sdwfs_number_count_dist = ''
+sdwfs_number_count_dist = f'{prefix}Data/Data_Repository/Project_Data/SPT-IRAGN/SDWFS_background/' \
+                          f'SDWFS_number_count_distribution_normed.json'
 
 # Clusters to manually exclude
 sdwfs_cutouts_to_exclude = {'SDWFS_cutout_026', 'SDWFS_cutout_028', 'SDWFS_cutout_046', 'SDWFS_cutout_049',
@@ -55,10 +58,12 @@ ch1_ch2_color = 0.7  # Minimum [3.6] - [4.5] color
 output_catalog = f'{prefix}Data/Output/SDWFS_cutout_IRAGN.fits'
 
 # Requested columns for output catalog
-output_column_names = ['SPT_ID', 'SZ_RA', 'SZ_DEC', 'ALPHA_J2000', 'DELTA_J2000', 'RADIAL_SEP_ARCMIN',
-                       'REDSHIFT', 'REDSHIFT_UNC', 'M500', 'M500_uerr', 'M500_lerr', 'R500', 'RADIAL_SEP_R500',
-                       'I1_MAG_APER4', 'I1_MAGERR_APER4', 'I1_FLUX_APER4', 'I1_FLUXERR_APER4', 'I2_MAG_APER4',
-                       'I2_MAGERR_APER4', 'I2_FLUX_APER4', 'I2_FLUXERR_APER4', 'COMPLETENESS_CORRECTION', 'MASK_NAME']
+output_column_names = ['Cutout_ID', 'SZ_RA', 'SZ_DEC', 'SDWFS_ID', 'ALPHA_J2000', 'DELTA_J2000',
+                       'I1_FLUX_APER4', 'I2_FLUX_APER4', 'I3_FLUX_APER4', 'I4_FLUX_APER4',
+                       'I1_FLUXERR_APER4', 'I2_FLUXERR_APER4', 'I3_FLUXERR_APER4', 'I4_FLUXERR_APER4',
+                       'I1_MAG_APER4', 'I2_MAG_APER4', 'I3_MAG_APER4', 'I4_MAG_APER4',
+                       'I1_MAGERR_APER4', 'I2_MAGERR_APER4', 'I3_MAGERR_APER4', 'I4_MAGERR_APER4',
+                       'COMPLETENESS_CORRECTION', 'MASK_NAME']
 
 # Run the pipeline.
 print('Starting Pipeline.')
@@ -82,22 +87,36 @@ sdwfs_selector.object_selection(ch1_bright_mag=ch1_bright_mag, ch2_bright_mag=ch
                                 absolute_mag=None,
                                 ch1_ch2_color_cut=ch1_ch2_color)
 sdwfs_selector.purify_selection(ch1_ch2_color_cut=ch1_ch2_color)
-sdwfs_selector.object_separations()
 sdwfs_selector.completeness_value()
-sdwfs_iragn_catalog = sdwfs_selector.final_catalogs()
+sdwfs_iragn_catalog = sdwfs_selector.final_catalogs(catalog_cols=output_column_names)
+
+# Instead of messing with the object separation function in the selector, we will compute the separations in post
+radial_sep_arcmin = []
+sdwfs_iragn_catalog_grp = sdwfs_iragn_catalog.group_by('Cutout_ID')
+for cutout in sdwfs_iragn_catalog_grp.groups:
+    center_coord = SkyCoord(cutout['SZ_RA'][0], cutout['SZ_DEC'][0], unit=u.deg)
+    object_coords = SkyCoord(cutout['ALPHA_J2000'], cutout['DELTA_J2000'], unit=u.deg)
+
+    # Compute separations
+    separations_arcmin = object_coords.separation(center_coord).to(u.arcmin)
+    radial_sep_arcmin.append(separations_arcmin)
+# Store the separations in the catalog
+sdwfs_iragn_catalog['RADIAL_SEP_ARCMIN'] = np.concatenate(radial_sep_arcmin)
+
+# Write it to disk
+sdwfs_iragn_catalog.write(output_catalog, overwrite=True)
 
 print('Full pipeline finished. Run time: {:.2f}s'.format(time() - pipeline_start_time))
 
 # List catalog statistics
 # SDWFS
-sdwfs_agn_catalog_grp = sdwfs_iragn_catalog.group_by('SPT_ID')
-number_of_cutouts_sdwfs = len(sdwfs_agn_catalog_grp.groups.keys)
-total_number_sdwfs = len(sdwfs_agn_catalog_grp)
-total_number_corrected_sdwfs = sdwfs_agn_catalog_grp['COMPLETENESS_CORRECTION'].sum()
-number_per_cluster_sdwfs = total_number_corrected_sdwfs / number_of_cutouts_sdwfs
+number_of_cutouts_sdwfs = len(sdwfs_iragn_catalog_grp.groups.keys)
+total_number_sdwfs = len(sdwfs_iragn_catalog_grp)
+total_number_corrected_sdwfs = sdwfs_iragn_catalog_grp['COMPLETENESS_CORRECTION'].sum()
+number_per_cutout_sdwfs = total_number_corrected_sdwfs / number_of_cutouts_sdwfs
 
-print(f"""SPT-SZ
+print(f"""SDWFS
 Number of clusters:\t{number_of_cutouts_sdwfs}
 Objects selected:\t{total_number_sdwfs}
 Objects selected (completeness corrected):\t{total_number_corrected_sdwfs:.2f}
-Objects per cluster (corrected):\t{number_per_cluster_sdwfs:.2f}""")
+Objects per cutout (corrected):\t{number_per_cutout_sdwfs:.2f}""")