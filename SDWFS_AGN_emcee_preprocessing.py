86c538339796fe68f135858b0ab115c54e4e37c8
Benjamin Floyd <benjaminfloyd7@gmail.com>
2020-09-22 18:00:38 -0500

    Added SDWFS_AGN_emcee_preprocessing.py and SDWFS_AGN_emcee_sampler_MPI.py to run MCMC chains on the SDWFS sample with a background-only model. The posterior from these chains will be used at the prior probability distribution for the background in the full cluster + background chains.

---
+++ SDWFS_AGN_emcee_preprocessing.py
@@ -0,0 +1,189 @@
+"""
+SPT_AGN_emcee_preprocessing.py
+Author: Benjamin Floyd
+
+Performs the GPF and cluster dictionary construction as a preprocessing step to the MCMC sampling. Results are stored in
+a JSON file for later use.
+"""
+
+import json
+from time import time
+
+import astropy.units as u
+import numpy as np
+from astropy.cosmology import FlatLambdaCDM
+from astropy.io import fits
+from astropy.table import Table
+from astropy.wcs import WCS
+from schwimmbad import MPIPool
+from scipy.spatial.distance import cdist
+
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+
+def rebin(a, rebin_factor, wcs=None):
+    """
+    Rebin an image to the new shape and adjust the WCS
+    :param a: Original image
+    :param rebin_factor: rebinning scale factor
+    :param wcs: Optional, original WCS object
+    :return:
+    """
+    newshape = tuple(rebin_factor * x for x in a.shape)
+
+    assert len(a.shape) == len(newshape)
+
+    slices = [slice(0, old, float(old) / new) for old, new in zip(a.shape, newshape)]
+    coordinates = np.mgrid[slices]
+    indices = coordinates.astype('i')  # recast the coordinates to int32
+    new_image = a[tuple(indices)]
+
+    if wcs is not None:
+        new_wcs = wcs.deepcopy()
+        new_wcs.pixel_shape = new_image.shape  # Update the NAXIS1/2 values
+        new_wcs.wcs.cd /= rebin_factor  # Update the pixel scale
+
+        # Check if the WCS has a PC matrix which is what AstroPy generates. If it exists, just delete it and stick with
+        # the CD matrix as the majority of the images have that natively.
+        if new_wcs.wcs.has_pc():
+            del new_wcs.wcs.pc
+
+        # Transform the reference pixel coordinate
+        old_crpix = wcs.wcs.crpix
+        new_crpix = np.floor(old_crpix) / a.shape * new_image.shape + old_crpix - np.floor(old_crpix)
+        new_wcs.wcs.crpix = new_crpix
+
+        return new_image, new_wcs
+
+    return new_image
+
+
+def good_pixel_fraction(r, center, cluster_id, rescale_factor=None):
+    # Read in the mask file and the mask file's WCS
+    image, header = mask_dict[cluster_id]  # This is provided by the global variable mask_dict
+    image_wcs = WCS(header)
+
+    if rescale_factor is not None:
+        image, image_wcs = rebin(image, rescale_factor, wcs=image_wcs)
+
+    # From the WCS get the pixel scale
+    try:
+        assert image_wcs.pixel_scale_matrix[0, 1] == 0.
+        pix_scale = image_wcs.pixel_scale_matrix[1, 1] * image_wcs.wcs.cunit[1]
+    except AssertionError:
+        # The pixel scale matrix is not diagonal. We need to diagonalize first
+        cd = image_wcs.pixel_scale_matrix
+        _, eig_vec = np.linalg.eig(cd)
+        cd_diag = np.linalg.multi_dot([np.linalg.inv(eig_vec), cd, eig_vec])
+        pix_scale = cd_diag[1, 1] * image_wcs.wcs.cunit[1]
+
+    # Convert our center into pixel units
+    center_pix = image_wcs.wcs_world2pix(center['SZ_RA'], center['SZ_DEC'], 0)
+
+    # Convert our radius to pixels
+    r_pix = (r / pix_scale).decompose()
+    r_pix = r_pix.value
+
+    # Because we potentially integrate to larger radii than can be fit on the image we will need to increase the size of
+    # our mask. To do this, we will pad the mask with a zeros out to the radius we need.
+    # Find the width needed to pad the image to include the largest radius inside the image.
+    width = ((int(round(np.max(r_pix) - center_pix[1])),
+              int(round(np.max(r_pix) - (image.shape[0] - center_pix[1])))),
+             (int(round(np.max(r_pix) - center_pix[0])),
+              int(round(np.max(r_pix) - (image.shape[1] - center_pix[0])))))
+
+    # Insure that we are adding a non-negative padding width.
+    width = tuple(tuple([i if i >= 0 else 0 for i in axis]) for axis in width)
+
+    large_image = np.pad(image, pad_width=width, mode='constant', constant_values=0)
+
+    # Generate a list of all pixel coordinates in the padded image
+    image_coords = np.dstack(np.mgrid[0:large_image.shape[0], 0:large_image.shape[1]]).reshape(-1, 2)
+
+    # The center pixel's coordinate needs to be transformed into the large image system
+    center_coord = np.array(center_pix) + np.array([width[1][0], width[0][0]])
+    center_coord = center_coord.reshape((1, 2))
+
+    # Compute the distance matrix. The entries are a_ij = sqrt((x_j - cent_x)^2 + (y_i - cent_y)^2)
+    image_dists = cdist(image_coords, np.flip(center_coord)).reshape(large_image.shape)
+
+    # select all pixels that are within the annulus
+    good_pix_frac = []
+    for j in np.arange(len(r_pix) - 1):
+        pix_ring = large_image[np.where((r_pix[j] <= image_dists) & (image_dists < r_pix[j + 1]))]
+
+        # Calculate the fraction
+        good_pix_frac.append(np.sum(pix_ring) / len(pix_ring))
+
+    return good_pix_frac
+
+
+def generate_catalog_dict(cluster):
+    cutout_id = cluster['Cutout_ID'][0]
+    cutout_sz_cent = cluster['SZ_RA', 'SZ_DEC'][0]
+    cutout_completeness = cluster['COMPLETENESS_CORRECTION']
+    cutout_radial_arcmin = cluster['RADIAL_SEP_ARCMIN']
+
+    # Find the appropriate mesh step size.
+    mask_wcs = WCS(mask_dict[cutout_id][1])
+    try:
+        assert mask_wcs.pixel_scale_matrix[0, 1] == 0.
+        pix_scale = mask_wcs.pixel_scale_matrix[1, 1] * mask_wcs.wcs.cunit[1]
+    except AssertionError:
+        # The pixel scale matrix is not diagonal. We need to diagonalize first
+        cd = mask_wcs.pixel_scale_matrix
+        _, eig_vec = np.linalg.eig(cd)
+        cd_diag = np.linalg.multi_dot([np.linalg.inv(eig_vec), cd, eig_vec])
+        pix_scale = cd_diag[1, 1] * mask_wcs.wcs.cunit[1]
+
+    # Generate a radial integration mesh.
+    rall = np.arange(0., max_radius, pix_scale / rescale_fact)
+
+    # Compute the good pixel fractions
+    cluster_gpf_all = good_pixel_fraction(rall, cutout_sz_cent, cutout_id,
+                                          rescale_factor=rescale_fact)
+
+    # Select only the objects within the same radial limit we are using for integration.
+    radial_arcmin_maxr = cutout_radial_arcmin[cutout_radial_arcmin <= rall[-1]]
+    completeness_weight_maxr = cutout_completeness[cutout_radial_arcmin <= rall[-1]]
+
+    # Construct our cluster dictionary with all data needed for the sampler.
+    # Additionally, store only values in types that can be serialized to JSON
+    cluster_dict = {'gpf_rall': cluster_gpf_all, 'rall': list(rall), 'radial_arcmin_maxr': list(radial_arcmin_maxr),
+                    'completeness_weight_maxr': list(completeness_weight_maxr)}
+
+    return cutout_id, cluster_dict
+
+
+hcc_prefix = '/work/mei/bfloyd/SPT_AGN/'
+max_radius = 5.0 * u.arcmin  # Maximum integration radius in arcmin
+
+rescale_fact = 6  # Factor by which we will rescale the mask images to gain higher resolution
+
+# Read in the mock catalog
+sdwfs_catalog = Table.read(f'{hcc_prefix}Data/Output/SDWFS_IRAGN.fits')
+
+# Read in the mask files for each cluster
+sdwfs_catalog_grp = sdwfs_catalog.group_by('Cutout_ID')
+mask_dict = {cluster_id: fits.getdata(hcc_prefix + mask_file, header=True) for cluster_id, mask_file
+             in zip(sdwfs_catalog_grp.groups.keys['Cutout_ID'],
+                    sdwfs_catalog_grp['MASK_NAME'][sdwfs_catalog_grp.groups.indices[:-1]])}
+
+# Compute the good pixel fractions for each cluster and store the array in the catalog.
+print('Generating Good Pixel Fractions.')
+start_gpf_time = time()
+with MPIPool() as pool:
+    # if not pool.is_master():
+    #     pool.wait()
+    #     sys.exit(0)
+    pool_results = pool.map(generate_catalog_dict, sdwfs_catalog_grp.groups)
+
+    if pool.is_master():
+        catalog_dict = {cluster_id: cluster_info for cluster_id, cluster_info in filter(None, pool_results)}
+
+print('Time spent calculating GPFs: {:.2f}s'.format(time() - start_gpf_time))
+
+# Store the results in a JSON file to be used later by the MCMC sampler
+preprocess_file = 'SDWFS_IRAGN_preprocessing.json'
+with open(preprocess_file, 'w') as f:
+    json.dump(catalog_dict, f, ensure_ascii=False, indent=4)
---
+++ SDWFS_AGN_emcee_sampler_MPI.py
@@ -0,0 +1,225 @@
+"""
+SPT_AGN_emcee_sampler_MPI.py
+Author: Benjamin Floyd
+
+This script will preform the Bayesian analysis on the SPT-AGN data to produce the posterior probability distributions
+for all fitting parameters.
+"""
+import json
+import os
+from argparse import ArgumentParser
+from time import time
+
+import astropy.units as u
+import emcee
+import numpy as np
+from astropy.cosmology import FlatLambdaCDM
+from custom_math import trap_weight  # Custom trapezoidal integration
+from schwimmbad import MPIPool
+
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+
+def model_rate_opted(params):
+    """
+    Our generating model
+
+    Parameters
+    ----------
+    params : tuple of float
+        Model parameters.
+
+    Returns
+    -------
+    model : float
+        A surface density profile of objects in the field of view
+    """
+
+    # Unpack our parameters
+    C, = params
+
+    # Convert our background surface density from angular units into units of r500^-2
+    background = C / u.arcmin ** 2
+
+    # Our model rate is a surface density of objects in angular units (as we only have the background in angular units)
+    model = background
+
+    return model.value
+
+
+# Set our log-likelihood
+def lnlike(param):
+    lnlike_list = []
+    for cutout_id in catalog_dict:
+        # Get the good pixel fraction for this cluster
+        gpf_all = catalog_dict[cutout_id]['gpf_rall']
+
+        # Get the radial positions of the AGN
+        radial_arcmin_maxr = catalog_dict[cutout_id]['radial_arcmin_maxr']
+
+        # Get the completeness weights for the AGN
+        completeness_weight_maxr = catalog_dict[cutout_id]['completeness_weight_maxr']
+
+        # Get the radial mesh for integration
+        rall = catalog_dict[cutout_id]['rall']
+
+        # Compute the completeness ratio for this cluster
+        completeness_ratio = len(completeness_weight_maxr) / np.sum(completeness_weight_maxr)
+
+        # Compute the model rate at the locations of the AGN.
+        ni = model_rate_opted(param)
+
+        # Compute the full model along the radial direction.
+        # The completeness weight is set to `1` as the model in the integration is assumed to be complete.
+        nall = model_rate_opted(param)
+
+        # Use a spatial poisson point-process log-likelihood
+        cluster_lnlike = np.sum(np.log(ni * radial_arcmin_maxr)) - completeness_ratio * trap_weight(
+            nall * 2 * np.pi * rall, rall, weight=gpf_all)
+        lnlike_list.append(cluster_lnlike)
+
+    total_lnlike = np.sum(lnlike_list)
+
+    return total_lnlike
+
+
+# For our prior, we will choose uninformative priors for all our parameters and for the constant field value we will use
+# a gaussian distribution set by the values obtained from the SDWFS data set.
+def lnprior(params):
+    # Extract our parameters
+    C, = params
+
+    # Define all priors
+    if 0.0 <= C < 5.0:
+        C_lnprior = 0.0
+    else:
+        C_lnprior = -np.inf
+
+    # Assuming all parameters are independent the joint log-prior is
+    total_lnprior = C_lnprior
+
+    return total_lnprior
+
+
+# Define the log-posterior probability
+def lnpost(params):
+    lp = lnprior(params)
+
+    # Check the finiteness of the prior.
+    if not np.isfinite(lp):
+        return -np.inf
+
+    return lp + lnlike(params)
+
+
+hcc_prefix = '/work/mei/bfloyd/SPT_AGN/'
+# hcc_prefix = ''
+
+parser = ArgumentParser(description='Runs MCMC sampler')
+parser.add_argument('--restart', help='Allows restarting the chain in place rather than resetting the chain.',
+                    action='store_true')
+args = parser.parse_args()
+
+# Load in the prepossessing file
+preprocess_file = os.path.abspath('SDWFS_IRAGN_preprocessing.json')
+with open(preprocess_file, 'r') as f:
+    catalog_dict = json.load(f)
+
+# Go through the catalog dictionary and recasting the cluster's mass and r500 to quantities and recast the radial
+# position and completeness lists to arrays.
+for cutout_id, cluster_info in catalog_dict.items():
+    catalog_dict[cutout_id]['radial_arcmin_maxr'] = np.array(cluster_info['radial_arcmin_maxr'])
+
+# Set up our MCMC sampler.
+# Set the number of dimensions for the parameter space and the number of walkers to use to explore the space.
+ndim = 1
+nwalkers = 5
+
+# Also, set the number of steps to run the sampler for.
+nsteps = int(1e6)
+
+# We will initialize our walkers in a tight ball near the initial parameter values.
+pos0 = np.vstack([[np.random.uniform(0., 1.)  # C
+                   ]
+                  for i in range(nwalkers)])
+
+# Set up the autocorrelation and convergence variables
+autocorr = np.empty(nsteps)
+old_tau = np.inf  # For convergence
+
+with MPIPool() as pool:
+    # if not pool.is_master():
+    #     pool.wait()
+    #     sys.exit(0)
+
+    # Filename for hd5 backend
+    chain_file = 'emcee_chains_SDWFS_IRAGN.h5'
+    backend = emcee.backends.HDFBackend(chain_file, name='SDWFS_background_uniform_prior')
+    if not args.restart:
+        backend.reset(nwalkers, ndim)
+
+    # Stretch move proposal. Manually specified to tune the `a` parameter.
+    moves = emcee.moves.StretchMove(a=2.75)
+
+    # Initialize the sampler
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnpost, backend=backend, moves=moves, pool=pool)
+
+    # Run the sampler.
+    print('Starting sampler.')
+    start_sampler_time = time()
+
+    # Sample up to nsteps.
+    for index, sample in enumerate(sampler.sample(pos0, iterations=nsteps)):
+        # Only check convergence every 100 steps
+        if sampler.iteration % 100:
+            continue
+
+        # Compute the autocorrelation time so far.
+        # Using tol = 0 means we will always get an estimate even if it isn't trustworthy
+        tau = sampler.get_autocorr_time(tol=0)
+        autocorr[index] = np.mean(tau)
+
+        # Check convergence
+        converged = np.all(tau * 100 < sampler.iteration)
+        converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+        if converged:
+            print('Chains have converged. Ending sampler early.\nIteration stopped at: {}'.format(sampler.iteration))
+            break
+        old_tau = tau
+
+print('Sampler runtime: {:.2f} s'.format(time() - start_sampler_time))
+
+# Get the chain from the sampler
+labels = [r'$C$']
+
+try:
+    # Calculate the autocorrelation time
+    tau_est = sampler.get_autocorr_time()
+
+    tau = np.mean(tau_est)
+
+    # Remove the burn-in. We'll use ~3x the autocorrelation time
+    burnin = int(3 * tau)
+
+    # We will also thin by roughly half our autocorrelation time
+    thinning = int(tau // 2)
+
+except emcee.autocorr.AutocorrError:
+    tau_est = sampler.get_autocorr_time(quiet=True)
+    tau = np.mean(tau_est)
+
+    burnin = int(sampler.iteration // 3)
+    thinning = 1
+
+flat_samples = sampler.get_chain(discard=burnin, thin=thinning, flat=True)
+
+for i in range(ndim):
+    mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+    q = np.diff(mcmc)
+    print('{labels} = {median:.3f} +{upper_err:.4f} -{lower_err:.4f}'
+          .format(labels=labels[i].strip('$\\'), median=mcmc[1], upper_err=q[1], lower_err=q[0]))
+
+print('Mean acceptance fraction: {:.2f}'.format(np.mean(sampler.acceptance_fraction)))
+
+# Get estimate of autocorrelation time
+print('Autocorrelation time: {:.1f}'.format(tau))
