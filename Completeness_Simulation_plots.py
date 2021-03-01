be602aec237230b74213b23d5cc1c01ce316ec91
Benjamin Floyd <benjaminfloyd7@gmail.com>
2021-03-01 17:39:09 -0600

    Update to Completeness_Simulation_plots.py. fixes file pathnames to new directory structure, removes unneeded plots and now computes the desired completeness limit (now 85%) magnitude.

--- Completeness_Simulation_plots.py
+++ Completeness_Simulation_plots.py
@@ -9,27 +9,22 @@ import json
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
+from scipy.optimize import root_scalar
 
-sptsz_file = 'Data/Comp_Sim/Results/SPTSZ_I2_results_gaussian_fwhm2.02_corr-0.11_mag0.2.json'
-sptpol_file = 'Data/Comp_Sim/SPTpol/Results/SPTpol_I2_results_gaussian_fwhm2.02_corr-0.11_mag0.2.json'
-sptsz_rereduced_file = 'Data/Comp_Sim/rereduced/Results/SPTSZ_rereduced_I2_results_gaussian_fwhm2.02_corr-0.11_mag0.2.json'
-sptsz_rotated_file = 'Data/Comp_Sim/Results/SPTSZ_I2_results_gaussian_fwhm2.02_corr-0.11_mag0.2_rotated.json'
+sptsz_file = 'Data_Repository/Project_Data/SPT-IRAGN/Comp_Sim/SPT-SZ_2500d/Results/' \
+             'SPTSZ_I2_results_gaussian_fwhm2.02_corr-0.11_mag0.2.json'
+sptpol_file = 'Data_Repository/Project_Data/SPT-IRAGN/Comp_Sim/SPTpol_100d/Results/' \
+              'SPTpol_I2_results_gaussian_fwhm2.02_corr-0.11_mag0.2.json'
 
 # Load in the results
-with open(sptsz_file, 'r') as f_sptsz, \
-        open(sptpol_file, 'r') as f_sptpol, \
-        open(sptsz_rereduced_file, 'r') as f_rereduced, \
-        open(sptsz_rotated_file, 'r') as f_rotated:
+with open(sptsz_file, 'r') as f_sptsz, open(sptpol_file, 'r') as f_sptpol:
     sptsz = json.load(f_sptsz)
     sptpol = json.load(f_sptpol)
-    sptsz_rereduced = json.load(f_rereduced)
-    sptsz_rotated = json.load(f_rotated)
 
 # Remove the magnitude bin entry from all dictionaries
 mag_bins = sptsz.pop('magnitude_bins', None)[:-1]
 del sptpol['magnitude_bins']
-del sptsz_rereduced['magnitude_bins']
-del sptsz_rotated['magnitude_bins']
 
 # Remove any cluster we've already determined unusable
 spt_sz_clusters_to_exclude = {'SPT-CLJ0045-5757', 'SPT-CLJ0201-6051', 'SPT-CLJ0230-4427', 'SPT-CLJ0456-5623',
@@ -42,67 +37,48 @@ for cluster_id in sptpol_clusters_to_exclude:
     sptpol.pop(cluster_id, None)
 
 # Because the SPT-SZ results are stored using observed ids not the official ids, we need to load in a look-up dictionary
-with open('Data/SPT-SZ_observed_to_official_ids.json', 'r') as f, \
-        open('Data/SPT-SZ_official_to_observed_ids.json', 'r') as g:
+with open('Data_Repository/Project_Data/SPT-IRAGN/Misc/SPT-SZ_observed_to_official_ids.json', 'r') as f, \
+        open('Data_Repository/Project_Data/SPT-IRAGN/Misc/SPT-SZ_official_to_observed_ids.json', 'r') as g:
     obs_to_official_ids = json.load(f)
     official_to_obs_ids = json.load(g)
 
-for cluster_id in sptsz_rereduced.keys():
-    sptsz_rereduced[obs_to_official_ids.get(cluster_id, cluster_id)] = sptsz_rereduced.pop(cluster_id, None)
-
-del sptsz_rereduced['SPT-CLJ0002-5557']  # Missing in SPT-SZ results
-
-# Collate results from common clusters
-comparison = {cluster_id: [sptsz[official_to_obs_ids[cluster_id]], sptpol[cluster_id], sptsz_rereduced[cluster_id]]
-              for cluster_id in sptsz_rereduced}
-
+# Plot the SPT-SZ 2500d clusters
 fig, ax = plt.subplots()
 for curve in sptsz.values():
     ax.plot(mag_bins, curve, color='k', alpha=0.2)
 ax.plot(mag_bins, np.median(list(list(curve) for curve in sptsz.values()), axis=0), color='r')
 ax.set(title='SPT-SZ 4.5um Completeness Simulations', xlabel='Vega Magnitude', ylabel='Recovery Rate')
-fig.savefig('Data/Comp_Sim/Plots/SPT-SZ_I2_completeness_sim_curves.pdf', format='pdf')
+# fig.savefig('Data_Repository/Project_Data/SPT-IRAGN/Comp_Sim/SPT-SZ_2500d/Plots/SPT-SZ_I2_completeness_sim_curves.pdf')
 plt.show()
 
+# Plot the SPTpol 100d clusters
 fig, ax = plt.subplots()
 for curve in sptpol.values():
     ax.plot(mag_bins, curve, color='k', alpha=0.2)
 ax.plot(mag_bins, np.median(list(list(curve) for curve in sptpol.values()), axis=0), color='r')
 ax.set(title='SPTpol 4.5um Completeness Simulations', xlabel='Vega Magnitude', ylabel='Recovery Rate')
-# fig.savefig('Data/Comp_Sim/Plots/SPTpol_I2_completeness_sim_curves.pdf', format='pdf')
+# fig.savefig('Data_Repository/Project_Data/SPT-IRAGN/Comp_Sim/SPTpol_100d/Plots/SPTpol_I2_completeness_sim_curves.pdf')
 plt.show()
 
-# fig, axarr = plt.subplots(ncols=1, nrows=2, sharex='col', figsize=(8, 12))
-# for ax, (cluster_id, curves) in zip(axarr.flatten(), comparison.items()):
-#     ax.plot(mag_bins, curves[0], label='Targeted (0.86"/pix)')
-#     ax.plot(mag_bins, curves[1], label='SSDF (0.6"/pix)')
-#     ax.plot(mag_bins, curves[2], label='Rereduced Targeted (0.6"/pix)')
-#     ax.axvline(x=17.46, color='k', linestyle='--', alpha=0.2)
-#     ax.axhline(y=0.8, color='k', linestyle='--', alpha=0.2)
-#     ax.set(title=cluster_id, ylabel='Recovery Rate')
-#     ax.legend()
-# axarr[-1].set(xlabel='Vega Magnitude')
-# fig.savefig('Data/Comp_Sim/rereduced/Plots/Rereduction_Comparison_Plot.pdf', format='pdf')
+# For the clusters common to both surveys supersede the SPTpol/SSDF curves for the deeper, SPT-SZ/targeted obs. curves.
+sptpol_exclusive = {cluster_id: curve for cluster_id, curve in sptpol.items()
+                    if official_to_obs_ids.get(cluster_id, '') not in sptsz}
+combined_sample = {**sptsz, **sptpol_exclusive}
 
-fig, ax = plt.subplots()
-for cluster_id in sptsz_rotated:
-    ax.plot(mag_bins, sptsz[cluster_id], alpha=0.5, color='C0', label='Old Run')
-    ax.plot(mag_bins, sptsz_rotated[cluster_id], alpha=0.5, color='C2', label='New Run')
-ax.set(title='SPT-SZ 4.5um Completeness Simulations with Rotated WCSs', xlabel='Vega Magnitude', ylabel='Recovery Rate')
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[:2], labels[:2])
-fig.savefig('Data/Comp_Sim/Plots/SPT-SZ_I2_completeness_sim_curves_rotated_comparison.pdf')
-plt.show()
+# Compute the combined sample median curve and the 85% Completeness level
+median_curve = np.median(list(list(curve) for curve in combined_sample.values()), axis=0)
+median_interp = interp1d(mag_bins, median_curve)
+faint_end_cut = root_scalar(lambda x: median_interp(x) - 0.85, bracket=[16.5, 18.0]).root
+print(f'Faint-end magnitude cut for 85% completeness: {faint_end_cut:.2f}')
 
-# Replace the bad curves in the original SPT-SZ curves to the corrected curves from sptsz_rotated
-for cluster_id in sptsz_rotated:
-    del sptsz[cluster_id]
+# Plot the combined sample
 fig, ax = plt.subplots()
-for curve in sptsz.values():
+for curve in combined_sample.values():
     ax.plot(mag_bins, curve, color='k', alpha=0.2)
-for curve in sptsz_rotated.values():
-    ax.plot(mag_bins, curve, color='C2', alpha=0.5)
-ax.plot(mag_bins, np.median(list(list(curve) for curve in sptsz.values()), axis=0), color='r')
-ax.set(title='SPT-SZ 4.5um Completeness Simulations', xlabel='Vega Magnitude', ylabel='Recovery Rate')
-fig.savefig('Data/Comp_Sim/Plots/SPT-SZ_I2_completeness_sim_curves_highlight_corrected_rotated.pdf', format='pdf')
+ax.plot(mag_bins, median_curve, color='r')
+ax.axvline(x=faint_end_cut, ls='--', color='r', alpha=0.5)
+ax.axhline(y=0.85, ls='--', color='r', alpha=0.5)
+ax.plot(faint_end_cut, 0.85, marker='o', color='r')
+ax.set(title='SPTcl 4.5um Completeness Simulations', xlabel='Vega Magnitude', ylabel='Recovery Rate')
+fig.savefig('Data_Repository/Project_Data/SPT-IRAGN/Comp_Sim/SPTcl/Plots/SPTcl_I2_completeness_sim_curves.pdf')
 plt.show()
