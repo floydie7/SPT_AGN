a688bdd400b76ad9f3dbbbd94e6964c24c6e4c5a
Benjamin Floyd <bfloyd@mail.umkc.edu>
2022-02-15 17:40:12 -0600

    Added radial binning calculations. Improvements can still be made as the mass and radial codes are identical in structure so I could unify them into a single method that could either return the lists I want or act as a generator.

--- Frequentist_Analysis/SPTcl_mass_radial_freq_analysis.py
+++ Frequentist_Analysis/SPTcl_mass_radial_freq_analysis.py
@@ -66,11 +66,48 @@ radial_bin_edges = stats.mstats.mquantiles(sptcl['RADIAL_SEP_R500'], [0., 0.25, 
 z_binned_clusters = sptcl.group_by(np.digitize(sptcl['REDSHIFT'], bins=z_bin_edges))
 
 # Iterate through the redshift bins
-for cluster_z_grp in z_binned_clusters.groups:
+z_binned_results = {}
+
+
+def surface_density():
+    # Compute the (corrected) total number of AGN candidates along the line-of-sight to each cluster
+    los_counts = np.sum(cluster['COMPLETENESS_CORRECTION'] * cluster['SELECTION_MEMBERSHIP'])
+    # Compute the field expectation for this line-of-sight
+    field_counts = SDWFS_SURF_DEN * cluster['IMAGE_AREA'].quantity[0]
+    # Subtract the field counts from the line-of-sight
+    cluster_counts = los_counts - field_counts
+    # Compute the surface density of the cluster excess objects
+    cluster_surf_den = cluster_counts / cluster['IMAGE_AREA'].quantity[0]
+    # For the errors on our measurement, compute the Poisson errors of the line-of-sight and field counts
+    los_counts_uerr, los_counts_lerr = small_poisson(los_counts)
+    field_counts_uerr, field_counts_lerr = small_poisson(field_counts)
+    # Propagate the errors
+    cluster_counts_uerr = np.sqrt(los_counts_uerr ** 2 + field_counts_uerr ** 2)
+    cluster_counts_lerr = np.sqrt(los_counts_lerr ** 2 + field_counts_lerr ** 2)
+    # Compute the surface density errors
+    cluster_surf_den_uerr = cluster_counts_uerr / cluster['IMAGE_AREA'].quantity[0]
+    cluster_surf_den_lerr = cluster_counts_lerr / cluster['IMAGE_AREA'].quantity[0]
+    # Convert our cluster counts (and errors) from angular to physical units
+    cluster_surf_den_mpc2 = cluster_surf_den / cosmo.kpc_proper_per_arcmin(cluster['REDSHIFT'][0]) \
+        .to(u.Mpc / u.arcmin) ** 2
+    cluster_surf_den_uerr_mpc2 = cluster_surf_den_uerr / cosmo.kpc_proper_per_arcmin(cluster['REDSHIFT'][0]) \
+        .to(u.Mpc / u.arcmin) ** 2
+    cluster_surf_den_lerr_mpc2 = cluster_surf_den_lerr / cosmo.kpc_proper_per_arcmin(cluster['REDSHIFT'][0]) \
+        .to(u.Mpc / u.arcmin) ** 2
+    # Accumulate the surface densities and errors (lower, upper)
+    surf_den.append(cluster_surf_den_mpc2)
+    surf_den_err.append((cluster_surf_den_lerr_mpc2.value, cluster_surf_den_uerr_mpc2.value))
+
+
+for i, cluster_z_grp in enumerate(z_binned_clusters.groups):
     # Group the catalog according to the mass bins
     cluster_z_m_clusters = [cluster_z_grp[(logM_bin_edges[i] <= cluster_z_grp['logM500']) &
                                           (cluster_z_grp['logM500'] <= logM_bin_edges[i + 1])]
                             for i in range(len(logM_bin_edges) - 1)]
+
+    cluster_z_r_clusters = [cluster_z_grp[(radial_bin_edges[i] <= cluster_z_grp['RADIAL_SEP_R500']) &
+                                          (cluster_z_grp['RADIAL_SEP_R500'] <= radial_bin_edges[i + 1])]
+                            for i in range(len(radial_bin_edges) - 1)]
 
     # Iterate through the mass bins
     m_bin_surf_den, m_bin_surf_den_err = [], []
@@ -81,41 +118,7 @@ for cluster_z_grp in z_binned_clusters.groups:
             m_bin_surf_den_err.append(np.nan)
         else:
             for cluster in cluster_z_m_grp.group_by('SPT_ID').groups:
-                # Compute the (corrected) total number of AGN candidates along the line-of-sight to each cluster
-                los_counts = np.sum(cluster['COMPLETENESS_CORRECTION'] * cluster['SELECTION_MEMBERSHIP'])
-
-                # Compute the field expectation for this line-of-sight
-                field_counts = SDWFS_SURF_DEN * cluster['IMAGE_AREA'].quantity[0]
-
-                # Subtract the field counts from the line-of-sight
-                cluster_counts = los_counts - field_counts
-
-                # Compute the surface density of the cluster excess objects
-                cluster_surf_den = cluster_counts / cluster['IMAGE_AREA'].quantity[0]
-
-                # For the errors on our measurement, compute the Poisson errors of the line-of-sight and field counts
-                los_counts_uerr, los_counts_lerr = small_poisson(los_counts)
-                field_counts_uerr, field_counts_lerr = small_poisson(field_counts)
-
-                # Propagate the errors
-                cluster_counts_uerr = np.sqrt(los_counts_uerr ** 2 + field_counts_uerr ** 2)
-                cluster_counts_lerr = np.sqrt(los_counts_lerr ** 2 + field_counts_lerr ** 2)
-
-                # Compute the surface density errors
-                cluster_surf_den_uerr = cluster_counts_uerr / cluster['IMAGE_AREA'].quantity[0]
-                cluster_surf_den_lerr = cluster_counts_lerr / cluster['IMAGE_AREA'].quantity[0]
-
-                # Convert our cluster counts (and errors) from angular to physical units
-                cluster_surf_den_mpc2 = cluster_surf_den / cosmo.kpc_proper_per_arcmin(cluster['REDSHIFT'][0]) \
-                    .to(u.Mpc / u.arcmin) ** 2
-                cluster_surf_den_uerr_mpc2 = cluster_surf_den_uerr / cosmo.kpc_proper_per_arcmin(cluster['REDSHIFT'][0]) \
-                    .to(u.Mpc / u.arcmin) ** 2
-                cluster_surf_den_lerr_mpc2 = cluster_surf_den_lerr / cosmo.kpc_proper_per_arcmin(cluster['REDSHIFT'][0]) \
-                    .to(u.Mpc / u.arcmin) ** 2
-
-                # Accumulate the surface densities and errors (lower, upper)
-                surf_den.append(cluster_surf_den_mpc2)
-                surf_den_err.append((cluster_surf_den_lerr_mpc2.value, cluster_surf_den_uerr_mpc2.value))
+                surface_density()
 
             # Compute the mean surface density for the mass bin
             bin_mean_surf_den = np.nanmean(u.Quantity(surf_den))
@@ -127,4 +130,30 @@ for cluster_z_grp in z_binned_clusters.groups:
             m_bin_surf_den.append(bin_mean_surf_den)
             m_bin_surf_den_err.append(bin_surf_den_err)
 
+    # Stow the results for plotting later
+    z_binned_results[i] = {'mass_bin_surf_den': m_bin_surf_den, 'mass_bin_surf_den_err': m_bin_surf_den_err}
+
+    # Iterate over the radial bins
+    r_bin_surf_den, r_bin_surf_den_err = [], []
+    for cluster_z_r_grp in cluster_z_r_clusters:
+        surf_den, surf_den_err = [], []
+        if len(cluster_z_r_grp) == 0:
+            r_bin_surf_den.append(np.nan)
+            r_bin_surf_den_err.append(np.nan)
+        else:
+            for cluster in cluster_z_r_grp.group_by('SPT_ID').groups:
+                surface_density()
+
+            # Compute the mean surface density for the mass bin
+            bin_mean_surf_den = np.nanmean(u.Quantity(surf_den))
+
+            # Combine the surface density errors in quadrature divided by the number of clusters contributing to the bin
+            bin_surf_den_err = np.sqrt(np.nansum(np.power(surf_den_err, 2), axis=0)) / len(
+                surf_den_err) / u.Mpc ** 2
+
+            # Accumulate the bin surface density and errors
+            r_bin_surf_den.append(bin_mean_surf_den)
+            r_bin_surf_den_err.append(bin_surf_den_err)
+
+    z_binned_results[i].update({'radial_bin_surf_den': r_bin_surf_den, 'radial_bin_surf_den_err': r_bin_surf_den_err})
 
