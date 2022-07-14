Fixed error with pixel scale units (needed to be in arcsec) in mask making. Also changed the selection membership method to allow multiple selection thresholds as an array which will just compute multiple options as different columns.

AGN_Selection/SDWFS_Cutout_AGN_Pipeline.py
@@ -5,6 +5,7 @@ Author: Benjamin Floyd
 A unified IR-AGN selection pipeline for both the SPT-SZ + SPTpol 100d surveys.
 """
 import json
+import re
 from time import time
 
 import astropy.units as u
@@ -63,7 +64,8 @@ ch2_bright_mag = 10.45  # Bright-end 4.5 um magnitude
 ch2_faint_mag = 17.46  # Faint-end 4.5 um magnitude
 # ch1_ch2_color = 0.7  # Minimum [3.6] - [4.5] color
 
-
+# Output catalog file name
+output_catalog = f'{prefix}Data_Repository/Project_Data/SPT-IRAGN/Output/SDWFS_cutout_IRAGN_purity.fits'
 
 # Requested columns for output catalog
 output_column_names = ['SPT_ID', 'SZ_RA', 'SZ_DEC', 'SDWFS_ID', 'ALPHA_J2000', 'DELTA_J2000', 'RADIAL_SEP_ARCMIN',
@@ -83,53 +85,46 @@ with open(sdwfs_purity_color_threshold, 'r') as f:
 print('Starting Pipeline.')
 pipeline_start_time = time()
 
-sdwfs_catalogs = []
-for ch1_ch2_color in color_thresholds:
-    catalog_start_time = time()
-    # Output catalog file name
-    output_catalog = f'{prefix}Data_Repository/Project_Data/SPT-IRAGN/Output/SDWFS_cutout_IRAGN_{ch1_ch2_color}.fits'
+# Initialize the SDWFS AGN selector
+sdwfs_selector = SelectSDWFS(sextractor_cat_dir=sdwfs_catalog_directory, irac_image_dir=sdwfs_image_directory,
+                             region_file_dir=sdwfs_regions_directory, mask_dir=sdwfs_masks_directory,
+                             sdwfs_master_catalog=sdfws_master_cutout_catalog,
+                             completeness_file=sdwfs_completeness_sim_results,
+                             field_number_dist_file=sdwfs_number_count_dist,
+                             sed=polletta_qso2,
+                             irac_filter=f'{prefix}Data_Repository/filter_curves/Spitzer_IRAC/080924ch1trans_full.txt',
+                             j_band_filter=f'{prefix}Data_Repository/filter_curves/KPNO/KPNO_2.1m/FLAMINGOS/'
+                                           f'FLAMINGOS.BARR.J.MAN240.ColdWitness.txt')
 
-    # Initialize the SDWFS AGN selector
-    sdwfs_selector = SelectSDWFS(sextractor_cat_dir=sdwfs_catalog_directory, irac_image_dir=sdwfs_image_directory,
-                                 region_file_dir=sdwfs_regions_directory, mask_dir=sdwfs_masks_directory,
-                                 sdwfs_master_catalog=sdfws_master_cutout_catalog,
-                                 completeness_file=sdwfs_completeness_sim_results,
-                                 field_number_dist_file=sdwfs_number_count_dist,
-                                 sed=polletta_qso2,
-                                 irac_filter=f'{prefix}Data_Repository/filter_curves/Spitzer_IRAC/080924ch1trans_full.txt',
-                                 j_band_filter=f'{prefix}Data_Repository/filter_curves/KPNO/KPNO_2.1m/FLAMINGOS/'
-                                               f'FLAMINGOS.BARR.J.MAN240.ColdWitness.txt')
+# Run the SDWFS pipeline and return the catalog
+sdwfs_agn_catalog = sdwfs_selector.run_selection(included_clusters=None,
+                                                 excluded_clusters=sdwfs_cutouts_to_exclude,
+                                                 max_image_catalog_sep=max_separation,
+                                                 ch1_min_cov=sdwfs_ch1_min_coverage,
+                                                 ch2_min_cov=sdwfs_ch2_min_coverage,
+                                                 ch1_bright_mag=ch1_bright_mag,
+                                                 ch2_bright_mag=ch2_bright_mag,
+                                                 selection_band_faint_mag=ch2_faint_mag,
+                                                 ch1_ch2_color=color_thresholds,
+                                                 spt_colnames=None,
+                                                 output_colnames=output_column_names,
+                                                 output_name=None)
+sdwfs_agn_catalog.rename_column('SPT_ID', 'CUTOUT_ID')
+sdwfs_agn_catalog.write(output_catalog, overwrite=True)
 
-    # Run the SDWFS pipeline and return the catalog
-    sdwfs_agn_catalog = sdwfs_selector.run_selection(included_clusters=None,
-                                                     excluded_clusters=sdwfs_cutouts_to_exclude,
-                                                     max_image_catalog_sep=max_separation,
-                                                     ch1_min_cov=sdwfs_ch1_min_coverage,
-                                                     ch2_min_cov=sdwfs_ch2_min_coverage,
-                                                     ch1_bright_mag=ch1_bright_mag,
-                                                     ch2_bright_mag=ch2_bright_mag,
-                                                     selection_band_faint_mag=ch2_faint_mag,
-                                                     ch1_ch2_color=ch1_ch2_color,
-                                                     spt_colnames=None,
-                                                     output_colnames=output_column_names,
-                                                     output_name=None)
-    sdwfs_agn_catalog.rename_column('SPT_ID', 'CUTOUT_ID')
-    sdwfs_agn_catalog.write(output_catalog, overwrite=True)
-    sdwfs_catalogs.append(sdwfs_agn_catalog)
-    print(f'Catalog with color threshold: {ch1_ch2_color:.2f} made. Run time: {time() - catalog_start_time:.2f}s')
 print('Full pipeline finished. Run time: {:.2f}s'.format(time() - pipeline_start_time))
 
 # List catalog statistics
-for sdwfs_agn_catalog in sdwfs_catalogs:
-    # SDWFS
+# SDWFS
+for selection_membership_key in [colname for colname in sdwfs_agn_catalog.colnames if 'SELECTION_MEMBERSHIP' in colname]:
     number_of_cutouts_sdwfs = len(sdwfs_agn_catalog.group_by('CUTOUT_ID').groups.keys)
     total_number_sdwfs = len(sdwfs_agn_catalog)
     total_number_comp_corrected_sdwfs = sdwfs_agn_catalog['COMPLETENESS_CORRECTION'].sum()
     total_number_corrected_sdwfs = np.sum(sdwfs_agn_catalog['COMPLETENESS_CORRECTION']
-                                          * sdwfs_agn_catalog['SELECTION_MEMBERSHIP'])
+                                          * sdwfs_agn_catalog[selection_membership_key])
     number_per_cutout_sdwfs = total_number_corrected_sdwfs / number_of_cutouts_sdwfs
 
-    print(f"""SDWFS
+    print(f"""SDWFS ({selection_membership_key[-4:]})
     Number of cutouts:\t{number_of_cutouts_sdwfs}
     Objects selected:\t{total_number_sdwfs}
     Objects selected (completeness corrected):\t{total_number_corrected_sdwfs:.2f}