bd0d0dc03a0769e250a382c0839e8deb2b764e65
Benjamin Floyd <bfloyd@mail.umkc.edu>
2023-04-05 10:07:07 -0500

    Improvements on the mock catalog comparison plot script.

--- mock_chain_comparison_plots.py
+++ mock_chain_comparison_plots.py
@@ -23,7 +23,7 @@ with h5py.File(snr_583_chains_fname, 'r') as f, h5py.File(snr_20_chains_fname, '
     snr_20_chain_names = list(g.keys())
 
 # Filter for desired chains
-catalog_name = 'e4.00_z-1.00'
+catalog_name = 'e-5.00_z2.00'
 chain_qualifiers = 'wideEtaZeta'
 snr_583_chain_names = [chain_name for chain_name in snr_583_chain_names
                        if (catalog_name in chain_name) and (chain_qualifiers in chain_name)]
@@ -41,25 +41,29 @@ burnin = 1500
 chains = {chain_name: sampler.get_chain(discard=burnin, flat=True) for chain_name, sampler in samplers.items()}
 
 # Pad the cluster-only chain with a null axis, so we can compare it with the others
-cl_only_name = 'mock_AGN_catalog_t0.0075_e4.00_z-1.00_b1.00_rc0.100_C0.181_eta_zeta_grid_308cl_snr5.83_wideEtaZeta_cl-only'
-chains[cl_only_name] = np.hstack((chains[cl_only_name],
-                                  np.full(chains[cl_only_name].shape[0], np.nan).reshape(
-                                      chains[cl_only_name].shape[0], 1)))
+if cl_only_chains := [s for s in list(chains.keys()) if 'cl-only' in s]:
+    for cl_only_name in cl_only_chains:
+        chains[cl_only_name] = np.hstack((chains[cl_only_name], np.full(chains[cl_only_name].shape[0], np.nan)
+                                          .reshape(chains[cl_only_name].shape[0], 1)))
 
 # %% Define plotting parameters
-chain_labels = [r'SNR$\sim$5.83', r'SNR$\sim$5.83 (a priori initialization)', r'SNR$\sim$5.83 (cluster only)',
-                r'SNR$\sim$20']
+chain_label_parser = re.compile(r'snr(\d+(?:.\d+)?)(?:_wideEtaZeta_)?(apriori|cl-only)?')
+chain_label_info = [chain_label_parser.findall(s)[0] for s in list(chains.keys())]
+chain_label_info = [(snr, suffix if suffix or snr != '5.83' else 'burnin') for snr, suffix in chain_label_info]
+suffix_opts = {'apriori': ' (a priori initialization)', 'cl-only': ' (cluster only)', 'burnin': ' (500 step burn-in)'}
+chain_labels = [rf'SNR$\sim${snr}{suffix_opts.get(suffix, "")}' for snr, suffix in chain_label_info]
 param_names = [r'$\theta$', r'$\eta$', r'$\zeta$', r'$\beta$', r'$r_c$', r'$C_0$']
 param_pattern = re.compile(r'(?:[tezbCx]|rc)(-*\d+.\d+|\d+)')
 truths = [np.array(param_pattern.findall(chain_name), dtype=float) for chain_name in chains.keys()]
-truth_colors = ['blue', 'orange', 'green', 'red']
-truth_ls = ['--', '--', '--', '--']
+truth_colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:len(chain_labels)]
+truth_ls = ['--'] * len(chain_labels)
 font_family = {'family': 'DejaVu Sans'}
 
 # Make plot
 fig = plotGTC(chains=list(chains.values()), chainLabels=chain_labels, paramNames=param_names, figureSize=8,
               truths=truths, truthColors=truth_colors, truthLineStyles=truth_ls,
               customLabelFont=font_family, customLegendFont=font_family, customTickFont=font_family)
-fig.suptitle(r'Mock Catalog ($\eta, \zeta$) = (4, -1)')
+fig.suptitle(rf'Mock Catalog ($\eta, \zeta$) = ({", ".join(param_pattern.findall(catalog_name))})')
 fig.savefig('Data_Repository/Project_Data/SPT-IRAGN/MCMC/Mock_Catalog/Plots/Port_Rebuild_Tests/eta-zeta_grid/snr_20/'
-            'corner_plot_e4_z-1_comparisons.pdf')
+            f'corner_plot_{catalog_name}_comparisons.pdf')
+fig.show()
