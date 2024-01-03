2c9e7db37ecdb49d577adb50e5453941e9ba8b36
Benjamin Floyd <benjaminfloyd7@gmail.com>
2024-01-03 12:15:50 -0600

    Split the background annulus radii determinations into a separate script as I needed the data for multiple other scripts. Adjustments have been made in the primary measurement script to reflect this change.

---
+++ SPT-local_bkg_annulus.py
@@ -0,0 +1,69 @@
+"""
+SPT-local_bkg_annulus.py
+Author: Benjamin Floyd
+
+Determines the necessary annulus outer radius used for the background measurement around the clusters. The annulus is
+determined iteratively for each cluster until the Poisson fractional error is approximately 5%.
+"""
+import glob
+import json
+import re
+
+import astropy.units as u
+import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy.cosmology import FlatLambdaCDM
+from astropy.table import QTable
+from tqdm import tqdm
+
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+cluster_id = re.compile(r'SPT-CLJ\d+-\d+')
+
+# Set our selection magnitude ranges
+ch1_bright_mag = 10.0  # Bright-end 3.6 um magnitude
+ch1_faint_mag = 18.3  # Faint-end 3.6 um magnitude
+ch2_bright_mag = 10.45  # Bright-end 4.5 um magnitude
+ch2_faint_mag = 17.48  # Faint-end 4.5 um magnitude
+
+# Set our background annulus ranges in terms of r200 radii
+inner_radius_factor = 3
+
+# Read in and process the SPT WISE galaxy catalogs
+spt_wise_gal_data = {}
+catalog_names = glob.glob('Data_Repository/Project_Data/SPT-IRAGN/local_backgrounds/catalogs/*_wise_local_bkg.ecsv')
+for catalog_name in tqdm(catalog_names, desc='Finding background annulus radii'):
+    spt_wise_gal = QTable.read(catalog_name)
+
+    # Select only objects within the magnitude ranges
+    spt_wise_gal = spt_wise_gal[(ch1_bright_mag < spt_wise_gal['w1mpro'].value) &
+                                (spt_wise_gal['w1mpro'].value <= ch1_faint_mag) &
+                                (ch2_bright_mag < spt_wise_gal['w2mpro'].value) &
+                                (spt_wise_gal['w2mpro'].value <= ch2_faint_mag)]
+
+    # Excise the cluster and only select objects in our chosen annulus
+    spt_wise_gal_cluster_coord = SkyCoord(spt_wise_gal['SZ_RA'][0], spt_wise_gal['SZ_DEC'][0], unit=u.deg)
+    spt_wise_gal_coords = SkyCoord(spt_wise_gal['ra'], spt_wise_gal['dec'], unit=u.deg)
+    spt_wise_gal_sep_deg = spt_wise_gal_cluster_coord.separation(spt_wise_gal_coords)
+
+    inner_radius_mpc = inner_radius_factor * spt_wise_gal['R200'][0]
+    inner_radius_deg = inner_radius_mpc * cosmo.arcsec_per_kpc_proper(spt_wise_gal['REDSHIFT'][0]).to(u.deg / u.Mpc)
+
+    outer_radius_deg = inner_radius_deg + 5 * u.arcsec
+    while True:
+        temp_cat = spt_wise_gal[(inner_radius_deg < spt_wise_gal_sep_deg) & (spt_wise_gal_sep_deg <= outer_radius_deg)]
+        frac_err = np.sqrt(len(temp_cat)) / len(temp_cat)
+
+        if np.isclose(frac_err, 0.05, atol=0.01, rtol=0.0):
+            break
+        else:
+            outer_radius_deg += 5 * u.arcsec
+
+    # Also compute the annulus area
+    spt_bkg_area = np.pi * (outer_radius_deg ** 2 - inner_radius_deg ** 2)
+
+    spt_wise_gal_data[cluster_id.search(catalog_name).group(0)] = {'inner_radius_deg': inner_radius_deg.value,
+                                                                   'outer_radius_deg': outer_radius_deg.value,
+                                                                   'annulus_area': spt_bkg_area.value}
+
+with open('Data_Repository/Project_Data/SPT-IRAGN/local_backgrounds/SPTcl-local_bkg_annulus.json', 'w') as f:
+    json.dump(spt_wise_gal_data, f)
--- SPT-local_bkg_measurement.py
+++ SPT-local_bkg_measurement.py
@@ -26,6 +26,7 @@ from astropy.table import QTable
 from scipy.integrate import simpson
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
+from tqdm import tqdm
 
 cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
 cluster_id = re.compile(r'SPT-CLJ\d+-\d+')
@@ -40,6 +41,12 @@ ch2_faint_mag = 17.48  # Faint-end 4.5 um magnitude
 mag_bin_width = 0.25
 magnitude_bins = np.arange(ch2_bright_mag, ch2_faint_mag, mag_bin_width)
 magnitude_bin_centers = magnitude_bins[:-1] + np.diff(magnitude_bins) / 2
+
+# Set our narrow magnitude ranges
+# (For fine-tuning scaling of SDWFS IRAC AGN to SPT WISE scaled "AGN")
+narrow_bright_mag = 15.
+narrow_faint_mag = 17.
+narrow_magnitude_filter = (narrow_bright_mag < magnitude_bin_centers) & (magnitude_bin_centers <= narrow_faint_mag)
 
 # Set our background annulus ranges in terms of r200 radii
 inner_radius_factor = 3
@@ -57,10 +64,15 @@ class ClusterInfo:
     wise_scaled_dndm_err: np.ndarray = None
 
 
+# Read in the annulus information that we previously calculated
+with open('Data_Repository/Project_Data/SPT-IRAGN/local_backgrounds/SPTcl-local_bkg_annulus.json', 'r') as f:
+    spt_wise_annuli_data = json.load(f)
+
 # Read in and process the SPT WISE galaxy catalogs
 spt_wise_gal_data = {}
 catalog_names = glob.glob('Data_Repository/Project_Data/SPT-IRAGN/local_backgrounds/catalogs/*_wise_local_bkg.ecsv')
-for catalog_name in catalog_names:
+for catalog_name in tqdm(catalog_names, desc='Processing SPT WISE Catalogs'):
+    cluster_name = cluster_id.search(catalog_name).group(0)
     spt_wise_gal = QTable.read(catalog_name)
 
     # Select only objects within the magnitude ranges
@@ -73,21 +85,16 @@ for catalog_name in catalog_names:
     spt_wise_gal_cluster_coord = SkyCoord(spt_wise_gal['SZ_RA'][0], spt_wise_gal['SZ_DEC'][0], unit=u.deg)
     spt_wise_gal_coords = SkyCoord(spt_wise_gal['ra'], spt_wise_gal['dec'], unit=u.deg)
     spt_wise_gal_sep_deg = spt_wise_gal_cluster_coord.separation(spt_wise_gal_coords)
-    spt_wise_gal_sep_mpc = (spt_wise_gal_sep_deg * cosmo.kpc_proper_per_arcmin(spt_wise_gal['REDSHIFT'][0])
-                            .to(u.Mpc / spt_wise_gal_sep_deg.unit))
 
-    inner_radius_mpc = inner_radius_factor * spt_wise_gal['R200'][0]
-    outer_radius_mpc = outer_radius_factor * spt_wise_gal['R200'][0]
-    inner_radius_deg = inner_radius_mpc * cosmo.arcsec_per_kpc_proper(spt_wise_gal['REDSHIFT'][0]).to(u.deg / u.Mpc)
-    outer_radius_deg = outer_radius_mpc * cosmo.arcsec_per_kpc_proper(spt_wise_gal['REDSHIFT'][0]).to(u.deg / u.Mpc)
+    # Retrieve the annulus radii and area
+    inner_radius_deg = spt_wise_annuli_data[cluster_name]['inner_radius_deg'] * u.deg
+    outer_radius_deg = spt_wise_annuli_data[cluster_name]['outer_radius_deg'] * u.deg
+    spt_bkg_area = spt_wise_annuli_data[cluster_name]['annulus_area'] * u.deg ** 2
 
-    spt_wise_gal = spt_wise_gal[(inner_radius_mpc < spt_wise_gal_sep_mpc) & (spt_wise_gal_sep_mpc <= outer_radius_mpc)]
+    # Select for the objects within the background annulus
+    spt_wise_gal = spt_wise_gal[(inner_radius_deg < spt_wise_gal_sep_deg) & (spt_wise_gal_sep_deg <= outer_radius_deg)]
 
-    # Also compute the annulus area
-    spt_bkg_area = np.pi * (outer_radius_deg ** 2 - inner_radius_deg ** 2)
-
-    spt_wise_gal_data[cluster_id.search(catalog_name).group(0)] = ClusterInfo(catalog=spt_wise_gal,
-                                                                              annulus_area=spt_bkg_area)
+    spt_wise_gal_data[cluster_name] = ClusterInfo(catalog=spt_wise_gal, annulus_area=spt_bkg_area)
 
 # Read in the SDWFS WISE galaxy--IRAC AGN scaling factor data
 with open('Data_Repository/Project_Data/SPT-IRAGN/SDWFS_background/'
@@ -104,7 +111,7 @@ z_bins = sdwfs_purity_data['redshift_bins'][:-1]
 agn_purity_color = interp1d(z_bins, sdwfs_purity_data['purity_90_colors'], kind='previous')
 
 # Compute the number count distribution of the WISE galaxies in the SPT background annulus
-for cluster_data in spt_wise_gal_data.values():
+for cluster_data in tqdm(spt_wise_gal_data.values(), desc='Computing dN/dm distributions'):
     catalog = cluster_data.catalog
     spt_bkg_area = cluster_data.annulus_area
     selection_color = agn_purity_color(catalog["REDSHIFT"][0])
@@ -138,9 +145,8 @@ for cluster_data in spt_wise_gal_data.values():
     cluster_data.sdwfs_irac_dndm_err = sdwfs_scaling_data[f'SELECTION_MEMBERSHIP_{selection_color:.2f}']['err']
 
 # For each cluster, renormalize the SDWFS number count distribution to match the scaled WISE galaxy level
-for cluster_data in spt_wise_gal_data.values():
-    # First, we need to only select a narrow range of magnitudes where the data is reliable
-    narrow_magnitude_filter = (15. < magnitude_bin_centers) & (magnitude_bin_centers <= 17.)
+for cluster_data in tqdm(spt_wise_gal_data.values(), desc='Renormalizing SDWFS dN/dm to local levels'):
+    # First, we need to only select objects within a narrow range of magnitudes where the data is reliable
     spt_dndm_narrow = cluster_data.wise_scaled_dndm[narrow_magnitude_filter]
     sdwfs_dndm_narrow = cluster_data.sdwfs_irac_dndm[narrow_magnitude_filter]
 
@@ -152,21 +158,27 @@ for cluster_data in spt_wise_gal_data.values():
     spt_dndm_symerr_narrow = np.sqrt(spt_dndm_err_narrow[0] * spt_dndm_err_narrow[1])
     sdwfs_dndm_symerr_narrow = np.sqrt(sdwfs_dndm_err_narrow[0] * sdwfs_dndm_err_narrow[1])
 
+    # NaNs can show up in errors if the counts in a bin were 0
+    # We want to handle this by setting the error to a large number so that it does not contribute to the fit
+    np.nan_to_num(spt_dndm_symerr_narrow, nan=1e64, copy=False)
+    np.nan_to_num(spt_dndm_symerr_narrow, nan=1e64, copy=False)
+
     # For our model fitting we will use errors that combine the two sources together
     combined_errors = np.sqrt(spt_dndm_symerr_narrow ** 2 + sdwfs_dndm_symerr_narrow ** 2)
 
     # Fit a simple model that relates the SDWFS IRAC and SPT scaled WISE distributions together
-    renorm_popt, remorm_pcov = curve_fit(lambda data, a: a * data, sdwfs_dndm_narrow, spt_dndm_narrow,
-                                         sigma=combined_errors)
+    renorm_popt, _ = curve_fit(lambda data, a: a * data, sdwfs_dndm_narrow, spt_dndm_narrow, sigma=combined_errors)
 
     # Apply the scaling factor to the data
     cluster_data.sdwfs_irac_dndm_scaled = cluster_data.sdwfs_irac_dndm * renorm_popt
 
+# print('Integrating dN/dm')
 # For each cluster integrate the scaled SDWFS number count distribution over the full selection magnitude range to find
 # the local background AGN surface density estimation
 spt_local_bkg_agn_surf_den = {cluster_name: simpson(cluster_data.sdwfs_irac_dndm_scaled, magnitude_bin_centers)
-                              for cluster_name, cluster_data in spt_wise_gal_data.items()}
+                              for cluster_name, cluster_data in tqdm(spt_wise_gal_data.items(),
+                                                                     desc='Integrating scaled SDWFS dN/dm')}
 
 # Write the results to file
-with open('Data_Repository/Project_Data/SPT-IRAGN/local_backgrounds/SPTcl-local_bkg.json', 'w') as f:
+with open('Data_Repository/Project_Data/SPT-IRAGN/local_backgrounds/SPTcl-local_bkg_frac_err.json', 'w') as f:
     json.dump(spt_local_bkg_agn_surf_den, f, cls=NumpyArrayEncoder)
