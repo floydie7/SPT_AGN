8d6217208b0f70bce2b5839fbbb89199b2258f48
Benjamin Floyd <benjamin.floyd@port.ac.uk>
2025-05-22 00:08:07 +0100

    Minor fix: Radial distribution column now respects if we are using mock or empirical catalogs.

--- SPT_AGN_emcee_preprocessing.py
+++ SPT_AGN_emcee_preprocessing.py
@@ -177,11 +177,14 @@ def generate_catalog_dict(cluster: Table) -> tuple[str, dict]:
     local_bkg_offset = local_bkg_surf_den - sdwfs_surf_den(agn_purity_color(cluster_z))
 
     # Set up a switch to handle the options for the radial separation
-    radial_switch = {0.0: cluster['RADIAL_SEP_R500'],
-                     0.5: cluster['RADIAL_SEP_R500_HALF_OFFSET'],
-                     0.75: cluster['RADIAL_SEP_R500_075_OFFSET'],
-                     1.0: cluster['RADIAL_SEP_R500_OFFSET']}
-    cluster_radial_r500 = radial_switch[args.miscentering]
+    if args.empirical:
+        cluster_radial_r500 = cluster['RADIAL_SEP_R500']
+    else:
+        radial_switch = {0.0: cluster['RADIAL_SEP_R500'],
+                         0.5: cluster['RADIAL_SEP_R500_HALF_OFFSET'],
+                         0.75: cluster['RADIAL_SEP_R500_075_OFFSET'],
+                         1.0: cluster['RADIAL_SEP_R500_OFFSET']}
+        cluster_radial_r500 = radial_switch[args.miscentering]
 
     # Determine the maximum integration radius for the cluster in terms of r500 units.
     max_radius_r500 = max_radius * cosmo.kpc_proper_per_arcmin(cluster_z).to(u.Mpc / u.arcmin) / cluster_r500
